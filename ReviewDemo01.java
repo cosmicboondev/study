53c5675687db5e0f6800adea9c83b92104726a1d
cosmicboondev <cosmicboon@inha.edu>
2021-09-17 22:56:05 +0900

    java01

---
+++ ReviewDemo01.java
@@ -0,0 +1,20 @@
+package javauniv;
+
+import java.util.Scanner;
+
+public class ReviewDemo01 {
+
+	public static void main(String[] args) {
+		// 소수: 1과 자기 자신외에는 나누어 떨어지지 않는 수
+		Scanner sc = new Scanner(System.in);
+		int number =  sc.nextInt();
+		int cnt = 0;
+		
+		for(int k =1; k<=number; k++){
+			if(number % k ==0)
+				cnt ++;
+		}
+
+	}
+
+}
